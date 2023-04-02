41b514c6db071cb1ba6ba7f5ea90e6f2c954e04c
Togrulx <tokhrulah@code.edu.az>
2023-04-02 14:10:24 +0400

    first

---
+++ hw2/Program.cs
@@ -0,0 +1,102 @@
+﻿Console.WriteLine();
+
+#region task1
+//Tələbə İmtahan Qiymətini Daxil Edir. Daxil Edilən İmtahan Qiyməti 0 və 100 Aralığında Olmalıdır.
+//Əgər Qiymət 91 və 100 Aralığındadırsa A Çap Etsin.
+//Əgər Qiymət 81 və 90 Aralığındadırsa B Çap Etsin.
+//Əgər Qiymət 71 və 80 Aralığındadırsa C Çap Etsin.
+//Əgər Qiymət 61 və 70 Aralığındadırsa D Çap Etsin.
+//Əgər Qiymət 51 və 60 Aralığındadırsa E Çap Etsin.
+//Əgər Qiymət 51 -dən Kiçikdirsə Kəsildiniz Çap Etsin
+
+
+
+
+//int point = 69;
+//if (point < 0 || point > 100)
+//{
+//    Console.WriteLine("DUZGUN BAL DAXIL EDIN");
+//}
+//else if (point <= 100 && point >= 91)
+//{
+//    Console.WriteLine("A");
+//}
+//else if (point < 90 && point >= 81)
+//{
+//    Console.WriteLine("B");
+//}
+//else if (point < 80 && point >= 71)
+//{
+//    Console.WriteLine("C");
+//}
+//else if (point < 70 && point >= 61)
+//{
+//    Console.WriteLine("D");
+//}
+//else if (point < 60 && point >= 51)
+//{
+//    Console.WriteLine("E");
+//}
+//else if (point <= 50)
+//{
+//    Console.WriteLine("kesildin");
+//}
+
+
+#endregion
+
+
+#region task2
+//Ədədlərdən ibarət olan arraydə neçə müsbət neçə mənfi ədəd olduğunu ekrana çıxardan alqoritm yazın.
+
+//int[] Numbers = { 1, 2, 3, 4, 5, -1, -2, -3, -4, -5 };
+//int musbet = 0;
+//int menfi = 0;
+//for (int i = 0; i < Numbers.Length; i++)
+//{
+//    if (Numbers[i] > 0)
+//    {
+//        musbet++;
+//    }
+//    else if (Numbers[i] < 0)
+//    {
+//        menfi++;
+//    }
+//}
+//Console.WriteLine(musbet);
+//Console.WriteLine(menfi);
+
+#endregion
+
+
+#region task3
+//Ədədlərdən ibarət olan bir array verilib həmin arraydə olan ən böyük ədədi ekrana yazdıran alqoritm yazın.
+
+
+//int[] Numbers = {89, 1, 2, 3, 4, 5, 258 };
+//int high = Numbers[0];
+//for (int i = 0; i < Numbers.Length; i++)
+//{
+//    if (Numbers[i] > high)
+//    {
+//         high = Numbers[i];
+//    }
+
+
+//}
+//Console.WriteLine(high);
+
+#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
